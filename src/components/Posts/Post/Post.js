7eda415ee1f1902f795f50db2675cbbbeffd4bad
rosenstein.samuel1 <rosenstein.samuel1@gmail.com>
2019-11-05 21:38:57 +0200

    need to fix posts

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import styles from './Post.module.css';
-import {
-    Card, CardText, CardBody,
-    CardTitle, Col
-  } from 'reactstrap';
+import React from "react";
+import styles from "./Post.module.css";
+import { Card, CardText, CardBody, CardTitle, Col } from "reactstrap";
 
-const post = (props) => (
-     
-         <Col className = {"col-md-4 d-flex align-items-stretch"}>
-          <div className = {styles.cards} >
-              {/* " sm="4"> */}
-         <Card body>
-            <CardBody> 
-                <CardTitle className={styles.cardTitle}><b>{props.title}</b></CardTitle>
-                <hr></hr>
-                <CardText>{props.content}</CardText>
-                <CardText>Author: {props.author}</CardText>
-            </CardBody>
-        </Card>
-          </div>
-         
-        </Col>
-)
+const post = props => (
+  <Col>
+    <div className={styles.cards}>
+      <Card body>
+        <CardBody>
+          <CardTitle className={styles.cardTitle}>
+            <b>{props.title}</b>
+          </CardTitle>
+          <hr></hr>
+          <CardText>{props.content}</CardText>
+          <CardText>Author: {props.author}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  </Col>
+);
 
 export default post;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,84 +1,73 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import * as firebase from "firebase";
-import config from '../../firebase-config'
-import Post from '../../components/Posts/Post/Post'
-import NewPost from '../../components/NewPost/NewPost'
-import styles from './Blog.module.css'
-import {Row} from 'reactstrap';
+import config from "../../firebase-config";
+import Post from "../../components/Posts/Post/Post";
+import NewPost from "../../components/NewPost/NewPost";
+import styles from "./Blog.module.css";
+import { CardColumns } from "reactstrap";
 
 class Blog extends Component {
-
   constructor(props) {
     super(props);
     // Post includes the following data:
     // Category, Title, Author, Location, Cotent
 
-    this.state= {
+    this.state = {
       posts: []
-    }
-//     this.state = {
-//           posts: [{id: 1, 
-//             title: 'used dirtbike',
-//             content: 'suzuki 2006. used dirtbike in good condition',
-//             author: 'sam'
-//           },
-//           {id: 1, 
-//             title: 'another used dirtbike',
-//             content: 'suzuki 2006. used dirtbike in good condition',
-//             author: 'sam'
-//           },
-//         ]
-//       }
+    };
 
     // Initialize Firebase
     if (!firebase.apps.length) {
-        firebase.initializeApp(config);
-     }
+      firebase.initializeApp(config);
+    }
   }
 
   componentDidMount() {
-    let postsRef = firebase.database().ref('posts');
-    let _this = this
-    postsRef.on('value', function(snapshot) {
+    let postsRef = firebase.database().ref("posts");
+    let _this = this;
+    postsRef.on("value", function(snapshot) {
       console.log(Object.values(snapshot.val()));
-  
+
       _this.setState({
         posts: Object.values(snapshot.val()),
         loading: false
-        });
+      });
     });
   }
 
   render() {
-
-    let posts = <p style={{textAlign: 'center'}}>Something went terribly wrong!</p>
+    let posts = (
+      <p style={{ textAlign: "center" }}>Something went terribly wrong!</p>
+    );
     if (!this.state.error) {
-        posts = this.state.posts.map(post => {
-            return <Post
-                title={post.title}
-                author={post.author}
-                content={post.content}
-            />;
-        });
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            title={post.title}
+            author={post.author}
+            content={post.content}
+          />
+        );
+      });
     }
 
     return (
       <div>
-          <div className={styles.appTitle}>
-            <h1>Cleveland Sports Fans- Unite</h1>
-            <h3>This is a forum for venting about the rough life we live as Cleveland sports fans.</h3>
-          </div>
-          <div>
-            <NewPost firebaseRef={firebase.database().ref('posts')}/>
-          </div>
-          <h3>Local Listings</h3> 
-          <Row className="cards">
-               {posts}
-          </Row>
+        <div className={styles.appTitle}>
+          <h1>Cleveland Sports Fans- Unite</h1>
+          <h3>
+            This is a forum for venting about the rough life we live as
+            Cleveland sports fans.
+          </h3>
+        </div>
+        <div>
+          <NewPost firebaseRef={firebase.database().ref("posts")} />
+        </div>
+        <h3>Local Listings</h3>
+        <CardColumns>{posts}</CardColumns>
       </div>
     );
   }
-  
 }
 
 export default Blog;
