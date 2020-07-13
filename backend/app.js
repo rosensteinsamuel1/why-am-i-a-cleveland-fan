5210c2884819f774097f117e4ff1e47f7eb3a4c3
rosenstein.samuel1 <rosenstein.samuel1@gmail.com>
2020-07-13 12:44:47 +0300

    reverting

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+<<<<<<< HEAD
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
     next();
 })
+=======
+app.use('/posts', postsRoutes);
+>>>>>>> parent of a6ffeff... finished basic backend functionality, get/post routes for coments
 
 // app.get('/comments', mongoPrac.getPosts);
 
